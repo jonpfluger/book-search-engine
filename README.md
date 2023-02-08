84611a84404b50adf2d071b867e237d92cadbcbb
jonpfluger <jonpfluger718@gmail.com>
2023-02-08 15:33:00 -0600

    updated readme with description and screenshots

--- README.md
+++ README.md
@@ -2,11 +2,25 @@
 
 ## Description
 
-test
+Most modern websites are driven by two things: data and user demands. The ability to personalize user data is the cornerstone of real-world web development today. As user demands evolve, applications need to be more performant.
+
+This application is a Google Books API search engine. I refactored the RESTful API into a GraphQL API using Apollo Server. The app was built using the MERN stack, with a React front end, MongoDB database, and Node.js/Express.js server and API. Allows users to save book searches to the back end.
+
+## Technologies Used
+
+- React
+- MongoDB/Mongoose
+- GraphQL
+- Apollo Server
+- Apollo Client
+- Node.js/Express.js
+
 
 ## Screenshots
 
-test
+![Picture of the book search application](./assets/mern-screenshot-01.gif)
+![Picture of the book search application](./assets/mern-screenshot-02.gif)
+![Picture of the book search application](./assets/mern-screenshot-03.gif)
 
 ## Deployed Link
 
--- server/server.js
+++ server/server.js
@@ -1,67 +1,32 @@
-// const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
-// const {ApolloServer} = require('apollo-server-express')
-// const {authMiddleware} = require('./utils/auth')
-// const {typeDefs, resolvers} = require('./schema');
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// const server = new ApolloServer({typeDefs, resolvers, context: authMiddleware})
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// // app.get("/", (req, res) => {
-// //   res.sendFile(path.join(__dirname, '../client/build/index.html'))
-// // })
-
-// app.use(routes);
-
-// const startApolloServer = async (typeDefs, resolvers) => {
-//   await server.start()
-//   server.applyMiddleware({app})
-// }
-
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`🌍 Now listening on localhost:${PORT}`)
-//     console.log(`GraphQL running ${server.graphqlPath}`)
-//   })
-// });
-
-// startApolloServer(typeDefs, resolvers)
-
-
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/'));
 })
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -72,6 +37,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
-  };
+};
+
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
